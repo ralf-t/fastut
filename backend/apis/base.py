af082d92604a4c7caf6b76a25e6ee35bbfd15f32
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-05 21:50:36 +0800

    part 14

---
+++ backend/apis/base.py
@@ -0,0 +1,11 @@
+from sys import prefix
+from fastapi import APIRouter
+
+from apis.v1 import (
+    route_general_pages,
+    route_users
+    )
+
+api_router = APIRouter()
+api_router.include_router(route_general_pages.general_pages_router, prefix='', tags=['general_pages'])
+api_router.include_router(route_users.router, prefix='/users', tags=['users'])
---
+++ readme.txt
@@ -0,0 +1,2 @@
+https://www.fastapitutorial.com/blog/fastapi-route/
+Follows a project design pattern called repository pattern
