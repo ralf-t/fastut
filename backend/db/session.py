ab169cc1f30ee2f697ec1905dfb9d49cfaacbe0b
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-05 21:21:26 +0800

    part 11-12

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from core.config import settings
 
@@ -12,5 +13,12 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
 )
 
-# the actual db session
+# the actual db sessionw
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
