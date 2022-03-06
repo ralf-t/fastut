c5f3c9f2284ba3b2365f555a2be0bf77ca9b6bc3
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-06 14:18:24 +0800

    p 16

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 
 from apis.v1 import (
     route_general_pages,
-    route_users
+    route_users,
+    route_jobs
     )
 
 api_router = APIRouter()
 api_router.include_router(route_general_pages.general_pages_router, prefix='', tags=['general_pages'])
 api_router.include_router(route_users.router, prefix='/users', tags=['users'])
+api_router.include_router(route_jobs.router, prefix='/jobs', tags=['jobs'])
