5848f495da170d076c9c58add61cfc51e5d24460
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-06 14:03:20 +0800

    p 15

---
+++ backend/tests/test_routes/test_users.py
@@ -0,0 +1,16 @@
+import json
+from urllib import response
+
+def test_create_user(client):
+    data = {
+        'username':'testuser',
+        'email':'testuser@nofoobar.com',
+        'password':'testing'
+    }
+
+    response = client.post('/users/', json.dumps(data))
+
+    assert response.status_code == 200
+    assert response.json()['email'] == 'testuser@nofoobar.com'
+    assert response.json()['is_active'] == True
+    
