c377fe3217a8e7a2b650c56fc136c9322e783e4c
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-06 14:25:10 +0800

    p 17

--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -15,3 +15,21 @@ def test_create_job(client):
     assert response.status_code == 200
     assert response.json()['company'] == 'doogle'
     assert response.json()['description'] == 'python'
+
+def test_read_job(client):
+    data = {
+        'title': 'SDE super',
+        'company': 'doogle',
+        'company_url': 'www.doogle.com',
+        'location': 'USA, NY',
+        'description': 'python',
+        'date_posted': '2022-03-20'
+    }
+
+    # posting again because every test, our db is fresh
+    response = client.post('/jobs/create-job/', json.dumps(data))
+
+    response = client.get('/jobs/get/1/')
+    
+    assert response.status_code == 200
+    assert response.json()['title'] == 'SDE super'
