d5e7e860cd8bf63616f59bc9ecf3eebe9819f112
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-08 00:39:51 +0800

    p19-20

--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -35,3 +35,40 @@ def test_read_job(client):
 
     assert response.status_code == 200
     assert response.json()["title"] == "SDE super"
+
+
+def test_read_all_jobs(client):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA, NY",
+        "description": "python",
+        "date_posted": "2022-03-20",
+    }
+
+    client.post("/jobs/create-job/", json.dumps(data))
+    client.post("/jobs/create-job/", json.dumps(data))
+
+    response = client.get("/jobs/all/")
+
+    assert response.status_code == 200
+    assert response.json()[0]
+    assert response.json()[1]
+
+
+def test_update_a_job(client):
+    data = {
+        "title": "New Job super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA, NY",
+        "description": "fastapi",
+        "date_posted": "2022-03-20",
+    }
+
+    client.post("/jobs/create-job/", json.dumps(data))
+    data["title"] = "test new title"
+    response = client.put("/jobs/update/1", json.dumps(data))
+
+    assert response.json()["msg"] == "Successfully updated data."
--- readme.txt
+++ readme.txt
@@ -2,3 +2,9 @@ https://www.fastapitutorial.com/blog/fastapi-route/
 Follows a project design pattern called repository pattern
 
 To avoid pre-commit hooks use:: git commit -m "my message" --no-verify
+
+To test codebase syntax:
+- 'pre-commit run --all-files'
+
+To test:
+- 'pytest'
